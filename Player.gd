1882fdfe6bae9f4474a177df4bec2952a806127d
조 형찬 <chan407@ajou.ac.kr>
2022-11-23 00:11:52 +0900

    feat : Add Player node, scene and set input map

---
+++ Player.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time
+# Same as setup() in Processing
+func _ready():
+	print("Hello Godot!")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
